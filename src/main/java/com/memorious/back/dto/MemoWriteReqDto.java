ddb6834136974d20b746761b96459cd44a4c755f
yoodeve <janhold@naver.com>
2023-11-06 13:41:08 +0900

    temp commit

--- src/main/java/com/memorious/back/dto/MemoWriteReqDto.java
+++ src/main/java/com/memorious/back/dto/MemoWriteReqDto.java
@@ -1,2 +1,12 @@
-package com.memorious.back.dto;public class MemoWriteReqDto {
+package com.memorious.back.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MemoWriteReqDto {
+    private String author;
+    private String memoContent;
+    private String createdDate;
 }
