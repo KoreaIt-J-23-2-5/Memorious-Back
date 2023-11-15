0625783443b25858e7c83e9ce4bb12f322447a81
SeongGwangJu <seonggwangdev@gmail.com>
2023-11-15 15:16:48 +0900

    feat: schedule Insert시 attendee_tb 정규화

--- src/main/java/com/memorious/back/repository/CalendarMapper.java
+++ src/main/java/com/memorious/back/repository/CalendarMapper.java
@@ -3,7 +3,10 @@ package com.memorious.back.repository;
 import com.memorious.back.entity.ScheduleEntity;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Map;
+
 @Mapper
 public interface CalendarMapper {
     public int insertSchedule(ScheduleEntity scheduleEntity);
+    public int insertAttendee(Map<String, Object> map);
 }
--- src/main/java/com/memorious/back/service/CalendarService.java
+++ src/main/java/com/memorious/back/service/CalendarService.java
@@ -5,16 +5,29 @@ import com.memorious.back.entity.ScheduleEntity;
 import com.memorious.back.repository.CalendarMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class CalendarService {
     private final CalendarMapper calendarMapper;
 
+    @Transactional(rollbackFor = Exception.class)
     public boolean addSchedule(ScheduleReqDto scheduleReqDto) {
-        ScheduleEntity scheduleEntity = scheduleReqDto.toEntity();
         System.out.println(scheduleReqDto);
-        return calendarMapper.insertSchedule(scheduleEntity) > 0;
+        ScheduleEntity scheduleEntity = scheduleReqDto.toEntity();
+        if(scheduleReqDto.getAttendee() == null || scheduleReqDto.getAttendee().toArray().length == 0) {
+            return calendarMapper.insertSchedule(scheduleEntity) > 0;
+        }
+        calendarMapper.insertSchedule(scheduleEntity);
+        Map<String, Object> map = new HashMap<>();
+        map.put("calendarScheduleId", scheduleEntity.getCalendarScheduleId());
+        map.put("userIdList", scheduleReqDto.getAttendee());
+        System.out.println(map);
+        return calendarMapper.insertAttendee(map) > 0;
     }
 
 }
